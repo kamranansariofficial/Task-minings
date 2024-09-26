6720989096cb6948cbd841f4c51ed41e47c4412f
Naveed17 <naveed07786@gmail.com>
2024-09-26 18:36:00 +0500

    add settings page

---
+++ src/app/[lang]/admin/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Settings from '@/components/_admin/settings'
+function SettingsPage() {
+    return (
+        <Settings />
+    )
+}
+
+export default SettingsPage
