d03e6970ec0b0b570a37499a68a73362496d6c62
Naveed17 <naveed07786@gmail.com>
2024-09-27 13:20:07 +0500

    fixed issues 1/*

---
+++ src/app/[lang]/admin/task/[taskId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import TaskDetails from '@/components/_admin/taskList/taskDetails'
+function TaskDetailsPage() {
+    return (
+        <><TaskDetails /></>
+    )
+}
+
+export default TaskDetailsPage
---
+++ src/app/[lang]/admin/task/add-new-task/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import UpdateTask from '@/components/_admin/taskList/updateTask'
+function AddTask() {
+    return (
+        <><UpdateTask /></>
+    )
+}
+
+export default AddTask
