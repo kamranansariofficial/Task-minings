794f04c6686f507ba3ec12f5c93ccc0c88a0ea8a
Kamran Ansari <49868934+kamranansariofficial@users.noreply.github.com>
2024-10-02 18:46:05 +0500

    asdasd

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,21 +2,11 @@
 import * as React from 'react';
 import ThemeRegistry from '@/ThemeRegistry/ThemeRegistry';
 import { Providers } from '@/lib/providers';
-import { i18n } from 'i18n-config';
 import { Toaster } from 'react-hot-toast';
-
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { LocaleFnsProvider } from '@/lib/localization';
-import { getLocale } from 'next-intl/server';
 
-export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }));
-}
-export const metadata = {
-  title: 'Task Minings',
-  description: 'Task Minings + Material UI v5',
-};
 
 export default function RootLayout({
   children,
